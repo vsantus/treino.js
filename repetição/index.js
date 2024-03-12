ab762686c7d6ab9705775b2466f1ebece7ee19ca
Vinicius Santos <viniciusantosd.abreu@gmail.com>
2024-03-12 14:56:08 -0300

    12/03

--- repetição/index.js
+++ repetição/index.js
@@ -1,14 +1,28 @@
 // laços de repetição: while do while e for
 
-var pergunta = 1
+// var pergunta = 1
+// var pergunta2 = 1
+var pergunta3 = 0
 
-// bloco de repetição com execução no começo
-// while (pergunta <= 7){ // enquanto pergunta for menor ou igual a 7, executa a pergunta do log
+//bloco de repetição com execução no começo
+// while (pergunta <= 6){ // enquanto pergunta for menor ou igual a 7, executa a pergunta do log
 //     console.log("Você quer café?");
 //     pergunta  ++
 // } 
 
-do{ // bloco de repetição com execução no final
-    console.log("Você quer café?")
-    pergunta++
-}while (pergunta <= 6)
+// bloco de repetição com execução no final
+// do{ 
+//     console.log("Você quer açucar?")
+//     pergunta2++
+// }while (pergunta2 <= 6)
+
+
+
+//inicializa recebendo a quantidade e valida, após isso ele verifica se a variavel é compativel com a ação <=, no fim ele age ate a variavel ser compativel
+console.log("Bom dia, ja olhou o cardápio?")
+for(pergunta3 = 1; pergunta3 <= 3; pergunta3++){
+    console.log('Eu quero pão')
+} // da pra repetir perguntas que estao em false ate que se torne true, pensar e treinar isso
+console.log("Seu pedido foi anotado, ok?")
+
+//entendi como usa o depurador (F10), ele mostra passo a passo do meu codigo, podendo testar cada parte dele
