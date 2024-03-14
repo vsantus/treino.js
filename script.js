56e6a81fa08b3a6aa80031acf80e03aebbb8eb49
Vinicius Santos <viniciusantosd.abreu@gmail.com>
2024-03-14 14:27:09 -0300

    14/03

---
+++ script.js
@@ -0,0 +1,20 @@
+// let teste = 5
+// let numero = 10
+
+// for(teste; teste <= numero; teste++){
+//     console.log(`exibição ${teste}`);
+// }
+
+let bateria = "Bateria esta carregando.." 
+let carga = true
+
+console.log(bateria);
+console.log("Bateria carregada. " + carga)
+
+let soma = 10 + 10
+
+console.log(soma)
+
+const texto = 'Vinicius ' + 'Santos'
+
+console.log(texto)
