b50148e35712049dcc78e8e7cbfd8ab93a903089
Vinicius Santos <viniciusantosd.abreu@gmail.com>
2024-04-03 12:48:17 -0300

    03/04

---
+++ funcoes/index.js
@@ -0,0 +1,25 @@
+const funcao = function parimp(n){
+    if (n%2 === 0){ 
+    return 'par'
+    }else{
+        return 'impar'
+    }
+} //função para verificar par ou impar de um valor inserido 
+ 
+console.log(funcao(20)); //funcao sendo chamada pela variavel
+
+ // *****************************************************************************************
+
+function soma(n1, n2){
+    return n1 + n2
+}
+
+console.log(soma(20,30)) // funcao normal retornando soma de valores
+
+ // *****************************************************************************************
+
+
+ //funçoes tem muita informação e da pra se aprofundar de diversas formas, existem muitas funcionalidades, pesquisar mais sobre !!
+
+
+
